748535c1ef557179ca4b2d2eb631c85803373138
Ubuntu <ubuntu@ip-172-31-45-147.us-west-2.compute.internal>
2016-03-17 08:43:57 +0000

    part1a

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/*readFileSync */
+var express = require('express')
+var app = express()
 
-var http = require('http');
-var fs = require('fs');
-var port = process.env.port || 8080
-http.createServer(function (request, response) {
-  	response.end(fs.readFileSync('index.html'),'utf8');
-  }).listen(port);
-console.log('Server running on port: ', port);
+/* serves all the static files*/
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
+
+/*serves main page*/
+app.get('/', function(request, response) {
+response.sendfile('index.html') 
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
