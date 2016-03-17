22302dbc0bd9952c25500f1916f8da21b649ca6e
Ubuntu <ubuntu@ip-172-31-45-147.us-west-2.compute.internal>
2016-03-17 08:48:10 +0000

    part1a

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var fs = require('fs');
+var index = 'index.html';
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
-
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// PART 1a
+function onRequest(req, res) {
+  var content = fs.readFileSync('index.html');
+  res.writeHeader(200, { 'Content-Type': 'text/html' });
+  res.write(content);
+  res.end();
+}
+http.createServer(onRequest).listen(process.env.PORT || 8000);
