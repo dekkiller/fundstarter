0a4a11037b0552fb47f751c58f38e95e63a2df69
Ubuntu <ubuntu@ip-172-31-45-147.us-west-2.compute.internal>
2016-03-17 08:56:03 +0000

    part1a final

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var fs = require('fs');
 var index = 'index.html';
 
 // PART 1a
-function onRequest(req, res) {
+/*function FundStarter(req, res) {
   var content = fs.readFileSync('index.html');
   res.writeHeader(200, { 'Content-Type': 'text/html' });
   res.write(content);
   res.end();
 }
-http.createServer(onRequest).listen(process.env.PORT || 8000);
+http.createServer(FundStarter).listen(process.env.PORT || 8000);
+*/
+
+
